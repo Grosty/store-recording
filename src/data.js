000ced2bfce2d4a4709d40fb1ed5fc31bd76d596
Grosty <grostmaster@gmail.com>
2020-02-27 15:51:34 +0200

    add increment and decrement methods

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Product title 1",
+        img: "img/product-1.png",
+        price: 10,
+        company: "Google",
+        info: "Lorem idsfsd sdfiosdf isdofnsdi sdoifsdf",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "Product title 2",
+        img: "img/product-2.png",
+        price: 12,
+        company: "Google",
+        info: "Lorem idsfsd sdfiosdf isdofnsdi sdoifsdf",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "Product title 3",
+        img: "img/product-3.png",
+        price: 13,
+        company: "Google",
+        info: "Lorem idsfsd sdfiosdf isdofnsdi sdoifsdf",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Product title 4",
+        img: "img/product-4.png",
+        price: 14,
+        company: "Google",
+        info: "Lorem idsfsd sdfiosdf isdofnsdi sdoifsdf",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: "Product title 5",
+        img: "img/product-5.png",
+        price: 15,
+        company: "Google",
+        info: "Lorem idsfsd sdfiosdf isdofnsdi sdoifsdf",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "Product title 6",
+        img: "img/product-6.png",
+        price: 16,
+        company: "Google",
+        info: "Lorem idsfsd sdfiosdf isdofnsdi sdoifsdf",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "Product title 7",
+        img: "img/product-7.png",
+        price: 17,
+        company: "Google",
+        info: "Lorem idsfsd sdfiosdf isdofnsdi sdoifsdf",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+];
+
+export const detailProduct = {
+    id: 1,
+    title: "Product title 1",
+    img: "img/product-1.png",
+    price: 10,
+    company: "Google",
+    info: "Lorem idsfsd sdfiosdf isdofnsdi sdoifsdf detail product object FFFFFFF ddd",
+    inCart: false,
+    count: 0,
+    total: 0
+};
